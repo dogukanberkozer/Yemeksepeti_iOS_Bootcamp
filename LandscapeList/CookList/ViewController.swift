7fbd759ed691dbc7eaf62cf152d986147d1886c6
Dogukan Berk OZER <38347138+dogukanberkozer@users.noreply.github.com>
2021-08-03 17:53:12 +0300

    Add files via upload

---
+++ LandscapeList/CookList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CookList
+//
+//  Created by Dogukan Berk Ozer on 3.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
